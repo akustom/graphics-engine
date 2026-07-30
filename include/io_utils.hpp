26473de520bb2d1a2ef04f2a0de0df54d5fa507e
akustom <dizzaastha@gmail.com>
2026-07-30 23:05:08 +0700

    refactored the code into winio, and made it connect with window instead of camera

--- include/io_utils.hpp
+++ include/io_utils.hpp
@@ -4,8 +4,6 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <GLFW/glfw3.h>
-#include "gfx/camera.hpp"
 
 namespace io {
     inline std::string readFile(const std::string& file_path) {
@@ -17,42 +15,5 @@ namespace io {
         std::stringstream buffer;
         buffer << file.rdbuf();
         return buffer.str();
-    }
-
-
-    inline void mouse_callback(GLFWwindow* window, const double x_pos, const double y_pos) {
-        if (glfwGetInputMode(window, GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
-            return;
-
-        auto* camera = static_cast<gfx::Camera*>(glfwGetWindowUserPointer(window));
-        if (!camera) return;
-
-        if (camera->firstMouse) {
-            camera->lastX = static_cast<float>(x_pos);
-            camera->lastY = static_cast<float>(y_pos);
-            camera->firstMouse = false;
-        }
-
-        const auto x_offset = static_cast<float>(x_pos - camera->lastX);
-        const auto y_offset = static_cast<float>(camera->lastY - y_pos);
-
-        camera->lastX = static_cast<float>(x_pos);
-        camera->lastY = static_cast<float>(y_pos);
-
-        camera->processMouse(x_offset, y_offset);
-    }
-
-    inline void mouse_button_callback(GLFWwindow* window, const int button, const int action, const int mods) {
-        if (button == GLFW_MOUSE_BUTTON_RIGHT) {
-            if (action == GLFW_PRESS) {
-                glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-                if (auto* camera = static_cast<gfx::Camera*>(glfwGetWindowUserPointer(window))) {
-                    camera->firstMouse = true;
-                }
-            }
-            else if (action == GLFW_RELEASE) {
-                glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-            }
-        }
     }
 }
---
+++ include/win/winio.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+#include "win/window.hpp"
+
+namespace io {
+    inline void cursor_pos_callback(GLFWwindow* window, const double x_pos, const double y_pos) {
+        if (glfwGetInputMode(window, GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
+            return;
+
+        auto* currentWindow = static_cast<win::Window*>(glfwGetWindowUserPointer(window));
+
+        if (!currentWindow->cursorContext.initialized) {
+            currentWindow->cursorContext.initialized = true;
+            currentWindow->cursorContext.lastX = static_cast<float>(x_pos);
+            currentWindow->cursorContext.lastY = static_cast<float>(y_pos);
+        }
+        currentWindow->cursorContext.offsetX = static_cast<float>(x_pos - currentWindow->cursorContext.lastX);
+        currentWindow->cursorContext.offsetY = static_cast<float>(currentWindow->cursorContext.lastY - y_pos);
+
+        currentWindow->cursorContext.lastX = static_cast<float>(x_pos);
+        currentWindow->cursorContext.lastY = static_cast<float>(y_pos);
+    }
+
+    inline void mouse_button_callback(GLFWwindow* window, const int button, const int action, const int mods) {
+        if (button == GLFW_MOUSE_BUTTON_RIGHT) {
+            auto* currentWindow = static_cast<win::Window*>(glfwGetWindowUserPointer(window));
+            if (action == GLFW_PRESS) {
+                glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+                currentWindow->cursorContext.initialized = false;
+            }
+            else if (action == GLFW_RELEASE) {
+                glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+            }
+        }
+    }
+}
