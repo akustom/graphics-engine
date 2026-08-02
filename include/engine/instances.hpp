0a0c660c4ac2389a2301a7ac89ff114d5d2c9e61
akustom <dizzaastha@gmail.com>
2026-08-02 16:26:30 +0700

    introduced instances

---
+++ include/engine/instances.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+
+#include "glm/fwd.hpp"
+
+#include "gfx/mesh.hpp"
+
+
+namespace engine {
+    struct Instances {
+        int instanceCount = 0;
+
+        std::vector<glm::vec4> positions;
+        std::vector<glm::vec4> velocities;
+        std::vector<glm::vec4> accelerations;
+
+        gfx::Mesh* mesh;
+
+        Instances(gfx::Mesh& instance_mesh) : mesh(&instance_mesh) {}
+
+        void bindToMesh(gfx::Mesh& l_mesh);
+
+        void createInstance(
+            const glm::vec3& position = {0, 0, 0},
+            const glm::vec3& velocity = {0, 0, 0},
+            const glm::vec3& acceleration = {0, 0, 0}
+            );
+    };
+}
---
+++ src/engine/instances.cpp
@@ -0,0 +1,18 @@
+#include "engine/instances.hpp"
+
+#include "glm/glm.hpp"
+
+
+namespace engine {
+    void Instances::bindToMesh(gfx::Mesh& l_mesh) {
+        mesh = &l_mesh;
+    }
+
+    void Instances::createInstance(const glm::vec3& position, const glm::vec3& velocity, const glm::vec3& acceleration) {
+        instanceCount++;
+
+        positions.emplace_back(position, 0);
+        velocities.emplace_back(velocity, 0);
+        accelerations.emplace_back(acceleration, 0);
+    }
+}
