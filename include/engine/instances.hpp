6a291257e10bbb5bc7de407ec9107f7ee42b5d06
akustom <dizzaastha@gmail.com>
2026-08-04 20:41:09 +0700

    created a get instance function returning a referencing struct wrapped like an AoS object

--- include/engine/instances.hpp
+++ include/engine/instances.hpp
@@ -12,6 +12,14 @@ namespace gfx {
 
 namespace engine {
     struct Instances {
+        struct Instance {
+            glm::vec4& position;
+            glm::vec4& velocity;
+            glm::vec4& acceleration;
+
+            Instance(glm::vec4& pos, glm::vec4& vel, glm::vec4& accel) : position(pos), velocity(vel), acceleration(accel) {}
+        };
+
         int instanceCount = 0;
 
         std::vector<glm::vec4> positions;
@@ -23,5 +31,14 @@ namespace engine {
             const glm::vec3& velocity = {0, 0, 0},
             const glm::vec3& acceleration = {0, 0, 0}
             );
+
+        Instance getInstance(size_t index) {
+            return Instance{
+                positions[index],
+                velocities[index],
+                accelerations[index]
+            };
+        }
+
     };
 }
