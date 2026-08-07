1e442205c07fe3d97fdcb99650b245019aa8471f
akustom <dizzaastha@gmail.com>
2026-08-07 20:41:23 +0700

    made the buffers references

--- include/engine/renderer.hpp
+++ include/engine/renderer.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
 #include "engine/mesh_buffer.hpp"
-#include "engine/instances.hpp"
+#include "engine/instances_buffer.hpp"
 
 
 namespace engine {
     struct Renderer {
-        MeshBuffer* meshBuffer;
+        MeshBuffer& meshBuffer;
+        InstancesBuffer& instancesBuffer;
 
-        Renderer(MeshBuffer& buffer) : meshBuffer(&buffer) {}
+        Renderer(MeshBuffer& mesh_buffer, InstancesBuffer& instances_buffer) : meshBuffer(mesh_buffer), instancesBuffer(instances_buffer) {}
 
-        void bindToBuffer(MeshBuffer& buffer);
         void render(gfx::Mesh& mesh, Instances& instances) const;
     };
 }
