b72b4b09db5bb71b00fb34768cfe0da332d074b7
akustom <dizzaastha@gmail.com>
2026-08-07 18:15:50 +0700

    added drawinstancesbasevertexbaseinstances

--- include/glw/render.hpp
+++ include/glw/render.hpp
@@ -22,5 +22,16 @@ namespace glw {
         instance_count,
         vbo_offset);
     }
+
+    inline void drawInstancesBaseVertexBaseInstances(int instance_count, const int indices_count, const int ebo_offset, const int vbo_offset, const int instance_offset) {
+        glDrawElementsInstancedBaseVertexBaseInstance(
+            GL_TRIANGLES,
+            indices_count,
+            GL_UNSIGNED_INT,
+            reinterpret_cast<void*>(ebo_offset * sizeof(unsigned int)),
+            instance_count,
+            vbo_offset,
+            instance_offset);
+    }
 }
 
