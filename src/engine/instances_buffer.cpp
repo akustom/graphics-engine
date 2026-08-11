806fb7c99d893630dfba479076ec7a894768bb94
akustom <dizzaastha@gmail.com>
2026-08-11 18:44:56 +0700

    copied meshbuffer's dynamic memory allocator into the push method

--- src/engine/instances_buffer.cpp
+++ src/engine/instances_buffer.cpp
@@ -9,13 +9,32 @@ namespace engine {
         return indexedInstances[instances.id];
     }
 
-    void InstancesBuffer::push(Instances& instances) {
-        instancesBuffer.allocateBuffer(instances.positions, GL_DYNAMIC_STORAGE_BIT);
-        vertexFormat.attachBuffer(instancesBuffer, bindingPoint, 0, util::bytesof<glm::vec4>());
+    void InstancesBuffer::push(std::vector<glm::vec4>& instancesComponent) {
+        if (!batchHeader.size) {
+            if (batchHeader.size + instancesComponent.size() > batchHeader.capacity) {
+                batchHeader.capacity = 2 * std::max(batchHeader.size, static_cast<int>(instancesComponent.size()));
+
+                glw::VBO tempVBO;
+                tempVBO.allocateBuffer<glm::vec4>(batchHeader.capacity, GL_DYNAMIC_STORAGE_BIT);
+                tempVBO.copyData<glm::vec4>(instancesBuffer, batchHeader.size);
+
+                instancesBuffer = std::move(tempVBO);
+                vertexFormat.attachBuffer(instancesBuffer, bindingPoint, 0, util::bytesof<glm::vec4>());
+            }
+            instancesBuffer.pushData(static_cast<int>(util::bytesof<glm::vec4>()) * batchHeader.size, instancesComponent);
+
+        } else {
+            instancesBuffer.allocateBuffer(instancesComponent);
+            vertexFormat.attachBuffer(
+                instancesBuffer,
+                bindingPoint, 0,
+                util::bytesof<glm::vec4>()
+                );
+        }
     }
 
     void InstancesBuffer::index(Instances& instances) {
-        push(instances);
+        push(instances.positions);
 
         instances.id = static_cast<int>(indexedInstances.size());
         indexedInstances.emplace_back(
