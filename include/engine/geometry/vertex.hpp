c859e1e1b9422e611432835be421eb721133b376
akustom <dizzaastha@gmail.com>
2026-08-17 13:12:10 +0700

    got rid of custom constructor so its is aggregate or smth

--- include/engine/geometry/vertex.hpp
+++ include/engine/geometry/vertex.hpp
@@ -5,14 +5,8 @@
 
 namespace engine::geo {
     struct vertex {
-        glm::vec3 pos;
-        glm::vec3 color;
-        glm::vec3 normal;
-
-        vertex(
-            glm::vec3 pos = glm::vec3(0),
-            glm::vec3 color = glm::vec3(0),
-            glm::vec3 normal = glm::vec3(0))
-        : pos(pos), color(color), normal(normal) {}
+        glm::vec3 pos = glm::vec3(0);
+        glm::vec3 color = glm::vec3(0);
+        glm::vec3 normal = glm::vec3(0);
     };
 }
