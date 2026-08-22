9d09aa61afb6256b67594bcbac0bb1bfc0ac9a35
akustom <dizzaastha@gmail.com>
2026-08-22 21:57:02 +0700

    introduced glw::vector

---
+++ include/glw/structure/vector.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <vector>
+
+#include "glw/primitive/buffer.hpp"
+#include "util.hpp"
+
+
+namespace glw {
+    template <typename T>
+    struct vector {
+        struct IndexHeader {
+            int count;
+            int bufferOffset;
+
+            IndexHeader(int count, int buffer_offset) : count(count), bufferOffset(buffer_offset) {}
+        };
+
+        std::vector<IndexHeader> indexed;
+
+        int size = 0;
+        int capacity = 0;
+
+        Buffer buffer;
+
+        Buffer& getBuffer() {
+            return buffer;
+        }
+
+        IndexHeader operator[](int i) const {
+            return indexed[i];
+        }
+
+        void push(std::vector<T>& obj) {
+            buffer.pushData(static_cast<int>(util::bytesof<T>()) * size, obj);
+        }
+
+        void reserve_more(int obj_size) {
+            capacity = 2 * std::max(size, obj_size);
+
+            Buffer temp;
+
+            temp.allocateBuffer<T>(capacity, GL_DYNAMIC_STORAGE_BIT);
+            temp.copyData<T>(buffer, size);
+
+            buffer = std::move(temp);
+        }
+
+        void push_back(std::vector<T>& obj, auto attachFormat) {
+            if (size) {
+                if (size + obj.size() > capacity) {
+                    reserve_more(obj.size());
+                    attachFormat();
+                }
+                push(obj);
+
+            } else {
+                buffer.allocateBuffer(obj);
+                attachFormat();
+            }
+
+            indexed.emplace_back(
+                obj.size(),
+                size
+            );
+
+            size += obj.size();
+        }
+    };
+}
