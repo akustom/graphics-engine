d0f18b405b859c95757e6e4dfbec222cbd95fb6d
akustom <dizzaastha@gmail.com>
2026-08-30 20:25:21 +0700

    added rule of 5

--- include/glw/structure/id_allocator.hpp
+++ include/glw/structure/id_allocator.hpp
@@ -6,11 +6,26 @@
 namespace glw {
     struct IDAllocator {
         struct Handle {
-            IDAllocator& parent;
+            IDAllocator* parent;
             int id = -1;
 
-            Handle(IDAllocator& parent_alloc, int id) : parent(parent_alloc), id(id) {}
-            ~Handle() {parent.free(*this);}
+            Handle(IDAllocator& parent_alloc, int id) : parent(&parent_alloc), id(id) {}
+            ~Handle() {parent->free(*this);}
+
+            Handle(const Handle&) = delete;
+            Handle& operator=(const Handle&) = delete;
+
+            Handle(Handle&& other) noexcept : parent(other.parent), id(other.id) {
+                other.id = -1;
+            }
+
+            Handle& operator=(Handle&& other) noexcept {
+                if (this != &other) {
+                    std::swap(parent, other.parent);
+                    std::swap(id, other.id);
+                }
+                return *this;
+            }
         };
 
 
@@ -33,7 +48,7 @@ namespace glw {
             }
 
             nextFreeHandle++;
-            return Handle{*this, nextFreeHandle - 1};
+            return Handle{*this, nextFreeHandle--};
         }
     };
 }
