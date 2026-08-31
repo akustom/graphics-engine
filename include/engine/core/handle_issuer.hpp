f36ce0417f030c64e79d446e88060d1a2802eafc
akustom <dizzaastha@gmail.com>
2026-08-31 19:50:40 +0700

    renamed idallocator to handleissuer; moved to engine/core

---
+++ include/engine/core/handle_issuer.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <vector>
+
+
+namespace glw {
+    struct HandleIssuer {
+        struct Handle {
+            HandleIssuer* parent;
+            int id = -1;
+
+            Handle(HandleIssuer& parent_alloc, int id) : parent(&parent_alloc), id(id) {}
+            ~Handle() {parent->free(*this);}
+
+            Handle(const Handle&) = delete;
+            Handle& operator=(const Handle&) = delete;
+
+            Handle(Handle&& other) noexcept : parent(other.parent), id(other.id) {
+                other.id = -1;
+            }
+
+            Handle& operator=(Handle&& other) noexcept {
+                if (this != &other) {
+                    std::swap(parent, other.parent);
+                    std::swap(id, other.id);
+                }
+                return *this;
+            }
+        };
+
+
+        std::vector<int> oldFreeHandles;
+        int nextFreeHandle = 0;
+
+        void free(const Handle& handle) {
+            if (handle.id == -1)
+                return;
+
+            oldFreeHandles.push_back(handle.id);
+        }
+
+        Handle getUniqueHandle() {
+            if (!oldFreeHandles.empty()) {
+                int id = oldFreeHandles.back();
+                oldFreeHandles.pop_back();
+
+                return Handle{*this, id};
+            }
+
+            nextFreeHandle++;
+            return Handle{*this, nextFreeHandle - 1};
+        }
+    };
+}
