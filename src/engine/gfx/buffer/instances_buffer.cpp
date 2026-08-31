91396ab70faa8ad36ad18c7f8f61e79e940a816b
akustom <dizzaastha@gmail.com>
2026-08-31 23:59:18 +0700

    made methods return core::Handle added subscript operator overloads

--- src/engine/gfx/buffer/instances_buffer.cpp
+++ src/engine/gfx/buffer/instances_buffer.cpp
@@ -1,19 +1,31 @@
 #include "engine/gfx/buffer/instances_buffer.hpp"
 
 #include "util.hpp"
+#include "engine/core/handle_issuer.hpp"
 #include "engine/scene/instances.hpp"
 
 
 namespace engine::gfx {
-    int InstancesBuffer::push(std::vector<glm::vec4>& instancesComponent) {
-        return instancesHeaders.push_back(instancesComponent, vertexFormat,
+    const glw::HeaderPair<1>& InstancesBuffer::operator[](const core::Handle& handle) const {
+        return registry.at(handle);
+    }
+
+    void InstancesBuffer::push(std::vector<glm::vec4>& instancesComponent) {
+        instancesHeaders.push_back(instancesComponent, vertexFormat,
             instancesHeaders.getBuffer(),
             bindingPoint, 0,
             util::bytesof<glm::vec4>()
         );
     }
 
-    int InstancesBuffer::index(scene::Instances& instances) {
-        return push(instances.positions);
+    const core::Handle& InstancesBuffer::index(scene::Instances& instances) {
+        push(instances.positions);
+
+        auto [it, inserted] = registry.emplace(
+            issuer.getUnique(),
+            glw::HeaderPair{instancesHeaders.back()}
+        );
+
+        return it->first;
     }
 }
--- src/engine/gfx/buffer/mesh_buffer.cpp
+++ src/engine/gfx/buffer/mesh_buffer.cpp
@@ -5,26 +5,34 @@
 
 
 namespace engine::gfx {
-    int MeshBuffer::pushVertices(std::vector<geo::vertex>& vertices) {
-        return vertexHeaders.push_back(vertices, vertexFormat,
+    const glw::HeaderPair<2>& MeshBuffer::operator[](const core::Handle& handle) const {
+        return registry.at(handle);
+    }
+
+    void MeshBuffer::pushVertices(std::vector<geo::vertex>& vertices) {
+        vertexHeaders.push_back(vertices, vertexFormat,
             vertexHeaders.getBuffer(),
             bindingPoint, 0,
             util::bytesof<geo::vertex>());
     }
 
-    int MeshBuffer::pushIndices(std::vector<glm::uint>& indices) {
-        return indexHeaders.push_back(indices, vertexFormat, indexHeaders.getBuffer());
+    void MeshBuffer::pushIndices(std::vector<glm::uint>& indices) {
+        indexHeaders.push_back(indices, vertexFormat, indexHeaders.getBuffer());
     }
 
-    int MeshBuffer::push(geo::Mesh& mesh) {
-        int ver = pushVertices(mesh.vertices);
-        int ind = pushIndices(mesh.indices);
-
-        // do something like notify a synchronization mismatch
-        return ver;
+    void MeshBuffer::push(geo::Mesh& mesh) {
+        pushVertices(mesh.vertices);
+        pushIndices(mesh.indices);
     }
 
-    int MeshBuffer::index(geo::Mesh& mesh) {
-        return push(mesh);
+    const core::Handle& MeshBuffer::index(geo::Mesh& mesh) {
+        push(mesh);
+
+        auto [it, inserted] = registry.emplace(
+            issuer.getUnique(),
+            glw::HeaderPair{vertexHeaders.back(), indexHeaders.back()}
+        );
+
+        return it->first;
     }
 }
