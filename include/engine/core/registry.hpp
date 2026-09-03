1551588313286e775dd4a99d53e88c53af8bfd1c
akustom <dizzaastha@gmail.com>
2026-09-03 20:37:35 +0700

    fixed core::registry (is now stable-untested)

--- include/engine/core/registry.hpp
+++ include/engine/core/registry.hpp
@@ -5,58 +5,83 @@
 
 namespace engine::core {
     struct rHandle {
-        int id;
-        int gen;
+        int sparse_index;
+        int generation;
     };
 
     struct Registry {
         struct SparseEntry {
-            int denseIndex;
+            int dense_index;
             int generation;
         };
 
         std::vector<SparseEntry> sparse;
         std::vector<int> dense;
+        std::vector<int> dense_tsp;
 
         std::vector<int> free_list;
 
-        int at(rHandle handle) {
-            auto [handleID, handleGen] = handle;
-            auto [denseIndex, sparseGen] = sparse[handleID];
-
-            if (handleGen != denseIndex) {
-
-            }
-
-            return dense[denseIndex];
+        void push_dense(int sparse_index, const int& data) {
+            dense_tsp.push_back(sparse_index);
+            dense.push_back(data);
         }
 
-        rHandle create() {
+        void pop_dense() {
+            dense_tsp.pop_back();
+            dense.pop_back();
+        }
+
+        void modify_dense(int dense_index, int tsp_sparse_i, const int& dense_data) {
+            dense_tsp[dense_index] = tsp_sparse_i;
+            dense[dense_index] = dense_data;
+        }
+
+        [[nodiscard]] int at(rHandle handle) const {
+            auto [sparse_index, handle_gen] = handle;
+            auto [dense_index, sparse_gen] = sparse[sparse_index];
+
+            assert(handle_gen == sparse_gen && "Undefined handle used!"); // todo to be refactored off in future debug class
+
+            return dense[dense_index];
+        }
+
+        rHandle create(int data) {
             if (!free_list.empty()) {
                 int free_id = free_list.back();
                 free_list.pop_back();
+
+                sparse[free_id].dense_index = static_cast<int>(dense.size());
+
+                dense_tsp.push_back(free_id);
+                dense.push_back(data);
 
                 return rHandle{free_id, sparse[free_id].generation};
             }
 
             sparse.emplace_back(dense.size(), 0);
+
+            dense_tsp.push_back(static_cast<int>(sparse.size()) - 1);
+            dense.push_back(data);
+
             return rHandle{static_cast<int>(sparse.size()) - 1, 0};
         }
 
         void free(rHandle handle) {
-            auto [handleID, handleGen] = handle;
-            auto& [denseIndex, sparseGen] = sparse[handleID];
+            auto& entry = sparse[handle.sparse_index];
+            entry.generation++;
 
-            dense.erase(dense.begin() + denseIndex);
+            modify_dense(entry.dense_index,
+                dense_tsp.back(),
+                dense.back());
+            pop_dense();
 
+            sparse[dense_tsp[entry.dense_index]].dense_index = entry.dense_index;
+
+            free_list.push_back(handle.sparse_index);
         }
 
         void modify(rHandle handle, const int& data) {
-            auto& [denseIndex, sparseGen] = sparse[handle.id];
-                sparseGen++;
 
-            int& denseData = dense[denseIndex];
-                denseData = data;
         }
     };
 }
