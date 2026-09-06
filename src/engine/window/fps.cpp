4381970a6c0eda8ff383cfe1a397202d2a885773
akustom <dizzaastha@gmail.com>
2026-09-06 12:08:45 +0700

    moved timebegin/endperiod to main (ill get to the abstraction later)

--- src/engine/window/fps.cpp
+++ src/engine/window/fps.cpp
@@ -3,18 +3,13 @@
 #include <GLFW/glfw3.h>
 #include <thread>
 #include <chrono>
-#include <windows.h>
-#include <timeapi.h>
-#pragma comment(lib, "winmm.lib")
 
 
 namespace engine::win {
     void FrameTimer::setFPS(float fps) {
         static constexpr double eps = 0.0009;
 
-        timeBeginPeriod(1);
-            std::this_thread::sleep_for(std::chrono::duration<double>(1.0f/fps - (glfwGetTime() - fpsLastFrame) - eps));
-        timeEndPeriod(1);
+        std::this_thread::sleep_for(std::chrono::duration<double>(1.0f/fps - (glfwGetTime() - fpsLastFrame) - eps));
 
         fpsLastFrame = glfwGetTime();
     }
