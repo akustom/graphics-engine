ae13922c34befcf202521ef50461e975862c547f
akustom <dizzaastha@gmail.com>
2026-09-07 20:08:50 +0700

    made fps limiter use a hybrid design

--- src/engine/window/fps.cpp
+++ src/engine/window/fps.cpp
@@ -4,12 +4,18 @@
 #include <thread>
 #include <chrono>
 
+#include <immintrin.h>
+
+namespace stc = std::chrono;
 
 namespace engine::win {
     void FrameTimer::setFPS(float fps) {
-        static constexpr double eps = 0.0009;
+        static double eps = 1/fps * 0.175;
 
-        std::this_thread::sleep_for(std::chrono::duration<double>(1.0f/fps - (glfwGetTime() - fpsLastFrame) - eps));
+        double sleep_time = 1.0f/fps - (glfwGetTime() - fpsLastFrame);
+        std::this_thread::sleep_for(stc::duration<double>(sleep_time - eps));
+
+        while (1.0f/fps >= glfwGetTime() - fpsLastFrame) {_mm_pause();}
 
         fpsLastFrame = glfwGetTime();
     }
