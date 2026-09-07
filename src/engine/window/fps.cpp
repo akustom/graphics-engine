b90aeefc588180beb3162219d201462efe1920b7
akustom <dizzaastha@gmail.com>
2026-09-07 20:13:42 +0700

    just cleaned things up a little

--- src/engine/window/fps.cpp
+++ src/engine/window/fps.cpp
@@ -6,16 +6,19 @@
 
 #include <immintrin.h>
 
-namespace stc = std::chrono;
+namespace std_c = std::chrono;
 
 namespace engine::win {
     void FrameTimer::setFPS(float fps) {
         static double eps = 1/fps * 0.175;
 
         double sleep_time = 1.0f/fps - (glfwGetTime() - fpsLastFrame);
-        std::this_thread::sleep_for(stc::duration<double>(sleep_time - eps));
 
-        while (1.0f/fps >= glfwGetTime() - fpsLastFrame) {_mm_pause();}
+        std::this_thread::sleep_for(std_c::duration<double>(sleep_time - eps));
+
+        while (1.0f/fps >= glfwGetTime() - fpsLastFrame) {
+            _mm_pause();
+        }
 
         fpsLastFrame = glfwGetTime();
     }
