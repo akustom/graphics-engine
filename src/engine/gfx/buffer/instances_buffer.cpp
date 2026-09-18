870e81979a50f4bc983a299b6be34ea2bf9b9506
akustom <dizzaastha@gmail.com>
2026-09-18 16:41:27 +0700

    added src definitions for the modify method, change surrounding code to use the item_ prefix, in index, changed registry.push to push uint32_t

--- src/engine/gfx/buffer/instances_buffer.cpp
+++ src/engine/gfx/buffer/instances_buffer.cpp
@@ -5,8 +5,25 @@
 
 
 namespace engine::gfx {
-    const glw::HeaderPair<1>& InstancesBuffer::operator[](core::Handle handle) const {
-        return registry.at(handle);
+    glw::HeaderPair<1> InstancesBuffer::operator[](core::Handle handle) const {
+        return {instancesHeaders[registry.at(handle)]};
+    }
+
+    core::Handle InstancesBuffer::index(scene::Instances& instances) {
+        push(instances.instancesPos());
+
+        auto issued = registry.push(static_cast<uint32_t>(instancesHeaders.size()) - 1);
+
+        return issued;
+    }
+
+    void InstancesBuffer::modify(core::Handle handle, scene::Instances& instancesData) {
+        instancesHeaders.modify(
+            instancesHeaders[registry.at(handle)], instancesData.instancesPos(),
+            vertexFormat,
+                instancesHeaders.getBuffer(),
+                bindingPoint, 0,
+                sizeof(glm::vec4));
     }
 
     void InstancesBuffer::push(std::vector<glm::vec4>& instancesComponent) {
@@ -14,13 +31,5 @@ namespace engine::gfx {
             instancesHeaders.getBuffer(),
             bindingPoint, 0,
             sizeof(glm::vec4));
-    }
-
-    core::Handle InstancesBuffer::index(scene::Instances& instances) {
-        push(instances.instancesPos());
-
-        auto issued = registry.push({instancesHeaders.back()});
-
-        return issued;
     }
 }
