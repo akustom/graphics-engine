f92f7e71b3b50b93c4199b739a8a961fdbea91b8
akustom <dizzaastha@gmail.com>
2026-09-18 20:34:47 +0700

    made headerindex for readability changed registry to use headerindex (like instances buffer) added modify

--- include/engine/gfx/buffer/mesh_buffer.hpp
+++ include/engine/gfx/buffer/mesh_buffer.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <array>
+
 #include <glm/fwd.hpp>
 
 #include "glw/core/vao.hpp"
@@ -17,17 +19,23 @@ namespace engine::gfx {
     public:
         MeshBuffer(glw::VAO& vertex_format, unsigned int binding_point) :
             vertexFormat(vertex_format), bindingPoint(binding_point) {}
-        const glw::HeaderPair<2>& operator[](core::Handle handle) const;
+        glw::HeaderPair<2> operator[](core::Handle handle) const;
 
         core::Handle index(geo::Mesh& mesh);
+        void modify(core::Handle handle, geo::Mesh& mesh);
 
     private:
+        struct HeaderIndex {
+            uint32_t vertex_i;
+            uint32_t index_i;
+        };
+
         glw::VAO& vertexFormat;
 
         glw::vector<geo::vertex> vertexHeaders;
         glw::vector<glm::uint> indexHeaders;
 
-        core::registry<glw::HeaderPair<2>> registry;
+        core::registry<HeaderIndex> registry;
 
         unsigned int bindingPoint;
 
--- src/engine/gfx/buffer/mesh_buffer.cpp
+++ src/engine/gfx/buffer/mesh_buffer.cpp
@@ -6,18 +6,34 @@
 
 
 namespace engine::gfx {
-    const glw::HeaderPair<2>& MeshBuffer::operator[](core::Handle handle) const {
-        return registry.at(handle);
+    glw::HeaderPair<2> MeshBuffer::operator[](core::Handle handle) const {
+        return {
+            vertexHeaders[registry.at(handle).vertex_i],
+            indexHeaders[registry.at(handle).index_i]};
     }
 
     core::Handle MeshBuffer::index(geo::Mesh& mesh) {
         push(mesh);
 
         auto handle = registry.push({
-            vertexHeaders.back(),
-            indexHeaders.back()});
+            static_cast<uint32_t>(vertexHeaders.size()) - 1,
+            static_cast<uint32_t>(indexHeaders.size())  - 1});
 
         return handle;
+    }
+
+    void MeshBuffer::modify(core::Handle handle, geo::Mesh& mesh) {
+        vertexHeaders.modify(
+            vertexHeaders[registry.at(handle).vertex_i], mesh.vertices,
+            vertexFormat,
+            vertexHeaders.getBuffer(),
+            bindingPoint, 0,
+            sizeof(geo::vertex));
+
+        indexHeaders.modify(
+            indexHeaders[registry.at(handle).index_i], mesh.indices,
+            vertexFormat,
+            indexHeaders.getBuffer());
     }
 
     void MeshBuffer::pushVertices(std::vector<geo::vertex>& vertices) {
