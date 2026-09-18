391bbb83e38fb0a3e1070be0942cc80dfdf35f4a
akustom <dizzaastha@gmail.com>
2026-09-18 16:38:22 +0700

    added a item_ prefix to size/capacity, created a size function that returns the amount of instances pushed/index count, created modify method to update instances with one of arbitrary size

--- include/glw/structure/vector.hpp
+++ include/glw/structure/vector.hpp
@@ -22,9 +22,12 @@ namespace glw {
     struct vector {
         std::vector<IndexHeader> indexed;
         Buffer buffer;
-        std::size_t size     = 0;
-        std::size_t capacity = 0;
+        std::size_t item_size     = 0;
+        std::size_t item_capacity = 0;
 
+        [[nodiscard]] std::size_t size() const {
+            return indexed.size();
+        }
 
         IndexHeader operator[](std::size_t i) const {
             return indexed[i];
@@ -39,28 +42,28 @@ namespace glw {
         }
 
         void reserve(std::size_t capacity) {
-            this->capacity = capacity;
+            this->item_capacity = capacity;
 
             Buffer temp;
 
             temp.allocateBuffer<T>(capacity, GL_DYNAMIC_STORAGE_BIT);
-            temp.copyData<T>(buffer, size);
+            temp.copyData<T>(buffer, item_size);
 
             buffer = std::move(temp);
         }
 
         template <typename... Args>
         void buffer_push(std::vector<T>& obj, VAO& format, Args&&... args) {
-            if (size == 0) {
-                capacity = obj.size();
-                buffer.allocateBuffer(obj);
+            if (item_size == 0) {
+                item_capacity = obj.size();
+                buffer.allocateBuffer(obj, GL_DYNAMIC_STORAGE_BIT);
                 format.attachBuffer(std::forward<Args>(args)...);
             } else {
-                if (size + obj.size() > capacity) {
-                    reserve(2 * std::max(size, obj.size()));
+                if (item_size + obj.size() > item_capacity) {
+                    reserve(2 * std::max(item_size, obj.size()));
                     format.attachBuffer(std::forward<Args>(args)...);
                 }
-                buffer.pushData(sizeof(T) * size, obj);
+                buffer.pushData(sizeof(T) * item_size, obj);
             }
         }
 
@@ -70,8 +73,29 @@ namespace glw {
 
             indexed.emplace_back(
                 static_cast<uint32_t>(obj.size()),
-                static_cast<uint32_t>(size));
-            size += obj.size();
+                static_cast<uint32_t>(item_size));
+            item_size += obj.size();
+        }
+
+        template <typename... Args>
+        void modify(IndexHeader header, std::vector<T>& obj, VAO& format, Args&&... args) { // todo wip
+            if (header.count != static_cast<uint32_t>(obj.size())) {
+                uint32_t buffer_diff = static_cast<uint32_t>(obj.size()) - header.count;
+
+                item_size += buffer_diff;
+
+                buffer_push(obj, format, std::forward<Args>(args)...);
+
+                for (auto& idxH : indexed) {
+                    if (idxH.bufferOffset == header.bufferOffset)
+                        idxH.count = obj.size();
+
+                    if (idxH.bufferOffset > header.bufferOffset)
+                        idxH.bufferOffset += buffer_diff;
+                }
+            }
+
+            buffer.pushData(sizeof(T) * header.bufferOffset, obj);
         }
     };
 }
