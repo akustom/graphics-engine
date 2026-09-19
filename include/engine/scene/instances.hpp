0acda68aaa406fbd5eaca2f8d47224fcb484dead
akustom <dizzaastha@gmail.com>
2026-09-19 20:35:04 +0700

    added force & mass to instances created Parameters to allow named arguments in Instances::createInstance

--- include/engine/scene/instances.hpp
+++ include/engine/scene/instances.hpp
@@ -1,40 +1,74 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
 
 #include <glm/glm.hpp>
 
 
 namespace engine::scene {
     class Instances {
+        struct Parameters {
+            glm::vec3 pos = glm::vec3(0);
+            glm::vec3 vel = glm::vec3(0);
+            glm::vec3 accel = glm::vec3(0);
+            glm::vec3 force = glm::vec3(0);
+
+            float mass = 1.0f;
+        };
+
     public:
-        struct InstanceRef {
+        struct Instance {
             glm::vec4& position;
             glm::vec4& velocity;
             glm::vec4& acceleration;
+            glm::vec4& force;
 
-            InstanceRef(glm::vec4& pos, glm::vec4& vel, glm::vec4& accel) : position(pos), velocity(vel), acceleration(accel) {}
+            float& mass;
+
+            Instance(glm::vec4& pos, glm::vec4& vel, glm::vec4& accel, glm::vec4& force, float& mass) : position(pos), velocity(vel), acceleration(accel), force(force), mass(mass) {}
         };
 
-        InstanceRef operator[](std::size_t index); // getting an instance
+        struct Iterator {
+            Instances& container;
+            std::size_t index;
 
-        void createInstance(
-            const glm::vec3& position = {0, 0, 0},
-            const glm::vec3& velocity = {0, 0, 0},
-            const glm::vec3& acceleration = {0, 0, 0}
-            );
+            Instance operator*() const {
+                return container[index];
+            }
+
+            Iterator& operator++() {
+                ++index;
+                return *this;
+            }
+
+            bool operator!=(const Iterator& other) const {
+                return index != other.index;
+            }
+        };
+
+        Iterator begin() { return Iterator{*this, 0}; }
+        Iterator end()   { return Iterator{*this, instanceCount}; }
+
+        Instance operator[](std::size_t index); // getting an instance
+
+        void createInstance(Parameters test);
+        void clear();
 
         std::vector<glm::vec4>& instancesPos();
         std::vector<glm::vec4>& instancesVel();
         std::vector<glm::vec4>& instancesAccel();
+        std::vector<glm::vec4>& instancesForce();
+        std::vector<float>& instancesMasses();
 
-        std::size_t size() const;
+        [[nodiscard]] std::size_t size() const;
 
     private:
         std::vector<glm::vec4> positions;
-
         std::vector<glm::vec4> velocities;
         std::vector<glm::vec4> accelerations;
+        std::vector<glm::vec4> forces;
+        std::vector<float> masses;
 
         std::size_t instanceCount = 0;
     };
