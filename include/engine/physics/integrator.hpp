7d50a90f5ebe28624dcfe1ca48e3d8785fff1793
akustom <dizzaastha@gmail.com>
2026-09-19 21:21:31 +0700

    fixed bug where acceleration accumulates

--- include/engine/physics/integrator.hpp
+++ include/engine/physics/integrator.hpp
@@ -39,11 +39,11 @@ namespace engine::phy {
                 instances.instancesVel()[i] += 1.0f/2.0f * instances.instancesAccel()[i] * dt;
             }
 
+            clear_forces(instances);
             apply_forces<forces...>(instances);
             for (int i = 0; i < instances.size(); i++) { // update accel -> a(t + dt)
-                instances.instancesAccel()[i] += instances.instancesForce()[i]/instances.instancesMasses()[i];
+                instances.instancesAccel()[i] = instances.instancesForce()[i]/instances.instancesMasses()[i];
             }
-            clear_forces(instances);
 
             for (int i = 0; i < instances.size(); i++) { // update vel -> v(t + dt)
                 instances.instancesVel()[i] += 1.0f/2.0f * instances.instancesAccel()[i] * dt;
