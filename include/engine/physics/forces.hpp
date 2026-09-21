2e422d8e9f9d9d327505d849aedb3d6f4713dd84
akustom <dizzaastha@gmail.com>
2026-09-21 21:21:54 +0700

    added simd:: namespace prefix to simde intrinsic types

--- include/engine/physics/forces.hpp
+++ include/engine/physics/forces.hpp
@@ -12,36 +12,36 @@ namespace engine::phy {
         ZoneScoped;
         static constexpr float G = 0.0000000000667430f;
 
-        __m256 m256_g_constant = _mm256_set1_ps(G);
+        simd::__m256 m256_g_constant = _mm256_set1_ps(G);
 
         const std::size_t items = instances.size();
         const std::size_t tail  = items - items % simd::OFFSET_256::vec4;
 
-        for (int i = 0; i < items; i++) {
-            glm::vec4 self_pos = instances[i].position;
+        for (std::size_t i = 0; i < items; i++) {
+            glm::vec4 self_pos   = instances[i].position;
             glm::vec4 self_force = instances[i].force;
             float self_mass = instances[i].mass;
 
-            __m256 m256_self_pos  = simd::_mm256_set1_vec4(self_pos);
-            __m256 m256_self_mass = simd::_mm256_set1_vec4(self_mass);
+            simd::__m256 m256_self_pos  = simd::_mm256_set1_vec4(self_pos);
+            simd::__m256 m256_self_mass = simd::_mm256_set1_vec4(self_mass);
 
             for (std::size_t j = 0; j < tail; j += simd::OFFSET_256::vec4) {
-                __m256 m256_other_pos   = simd::_mm256_loadu_vec4(&instances.instancesPos()[j]);
-                __m256 m256_other_force = simd::_mm256_loadu_vec4(&instances.instancesForce()[j]);
-                __m256 m256_other_mass = simd::_mm256_alignf_vec4(
+                simd::__m256 m256_other_pos   = simd::_mm256_loadu_vec4(&instances.instancesPos()[j]);
+                simd::__m256 m256_other_force = simd::_mm256_loadu_vec4(&instances.instancesForce()[j]);
+                simd::__m256 m256_other_mass = simd::_mm256_alignf_vec4(
                     instances.instancesMasses()[j],
                     instances.instancesMasses()[j+1]);
 
-                __m256 m256_diff = simd::_mm256_sub_vec4(m256_self_pos, m256_other_pos); // self_pos - other_pos
-                __m256 m256_dir  = simd::_mm256_norm_vec4(m256_diff); // norm(self_pos - other_pos)
+                simd::__m256 m256_diff = simd::_mm256_sub_vec4(m256_self_pos, m256_other_pos); // self_pos - other_pos
+                simd::__m256 m256_dir  = simd::_mm256_norm_vec4(m256_diff); // norm(self_pos - other_pos)
 
-                __m256 m256_denom = simd::_mm256_length2_vec4(m256_diff); // mag(self_pos - other_pos)^2
+                simd::__m256 m256_denom = simd::_mm256_length2_vec4(m256_diff); // mag(self_pos - other_pos)^2
 
-                __m256 m256_numer = simd::_mm256_mul_vec4(m256_dir, m256_other_mass); // dir * m1
+                simd::__m256 m256_numer = simd::_mm256_mul_vec4(m256_dir, m256_other_mass); // dir * m1
                 m256_numer = simd::_mm256_mul_vec4(m256_numer, m256_self_mass); // dir * m1 * m2
                 m256_numer = simd::_mm256_mul_vec4(m256_numer, m256_g_constant); // dir * m1 * m2 * G
 
-                __m256 m256_force = simd::_mm256_div_vec4(m256_numer, m256_denom); // dir * m1 * m2 * G / mag^2
+                simd::__m256 m256_force = simd::_mm256_div_vec4(m256_numer, m256_denom); // dir * m1 * m2 * G / mag^2
                 m256_force = simd::_mm256_add_vec4(m256_other_force, m256_force);
 
                 simd::_mm256_storeu_vec4(&instances.instancesForce()[j], m256_force);
